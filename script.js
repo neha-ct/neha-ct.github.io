2d4210737571b7bdb8f9fc5f3c239a09e029062a
neha-ct <ct.neha.22@gmail.com>
2024-10-14 23:34:07 -0400

    Meta Data & Upcoming Event Slideshow
    Added meta tags such as keywords, author, description, etc. and added a upcoming event slideshow to the index page. Changes in HTML, style.css, and script.js.

--- script.js
+++ script.js
@@ -59,3 +59,32 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+let slideIndex2 = 1;
+showSlides(slideIndex2);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex2 += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex2 = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex2-1].style.display = "block";
+  dots[slideIndex2-1].className += " active";
+}
